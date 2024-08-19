671026435c5a87ba3beb36aeff13fd0a85321ad5
minecrafter8001 <133237713+Minecrafter8001@users.noreply.github.com>
2024-08-19 23:10:47 +0100

    disable BlockHighlighter.java

--- src/main/java/dev/su5ed/mffs/render/BlockHighlighter.java
+++ src/main/java/dev/su5ed/mffs/render/BlockHighlighter.java
@@ -40,60 +40,64 @@ public final class BlockHighlighter {
     public static final Color LIGHT_RED = new Color(1, 0, 0, 0.25F);
 
     public static void highlightBlock(PoseStack pose, Vec3 cameraPos, BlockPos pos, Color color) {
-        highlightArea(pose, cameraPos, Shapes.create(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1), color);
+        // Block highlighting is disabled.
+        // highlightArea(pose, cameraPos, Shapes.create(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1), color);
     }
 
     public static void highlightArea(PoseStack pose, Vec3 cameraPos, BlockPos from, BlockPos to) {
-        AABB area = AABB.encapsulatingFullBlocks(from, to);
-        VoxelShape shape = Shapes.create(area);
-        highlightArea(pose, cameraPos, shape, null);
+        // Block highlighting is disabled.
+        // AABB area = AABB.encapsulatingFullBlocks(from, to);
+        // VoxelShape shape = Shapes.create(area);
+        // highlightArea(pose, cameraPos, shape, null);
     }
 
     public static void highlightArea(PoseStack pose, Vec3 cameraPos, VoxelShape shape, @Nullable Color fillColor) {
-        Tesselator tessellator = Tesselator.getInstance();
-
-        // Setup rendering
-        RenderSystem.setShader(GameRenderer::getPositionColorShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.enableBlend();
-        RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        RenderSystem.disableDepthTest(); // See through
-        RenderSystem.disableCull();
-        RenderSystem.depthMask(false);
-
-        pose.pushPose();
-        pose.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z);
-        // Allow glass and other translucent/transparent objects to render properly
-        if (fillColor != null) {
-            drawOutlineBoxes(tessellator, pose, fillColor, shape);
-        }
-        drawOutlineLines(tessellator, pose, OUTLINE_COLOR, shape);
-        pose.popPose();
-
-        RenderSystem.depthMask(true);
-        RenderSystem.enableDepthTest();
-        RenderSystem.enableCull();
+        // Block highlighting is disabled.
+        // Tesselator tessellator = Tesselator.getInstance();
+        // 
+        // // Setup rendering
+        // RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        // RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        // RenderSystem.enableBlend();
+        // RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        // RenderSystem.disableDepthTest(); // See through
+        // RenderSystem.disableCull();
+        // RenderSystem.depthMask(false);
+        //
+        // pose.pushPose();
+        // pose.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z);
+        // // Allow glass and other translucent/transparent objects to render properly
+        // if (fillColor != null) {
+        //     drawOutlineBoxes(tessellator, pose, fillColor, shape);
+        // }
+        // drawOutlineLines(tessellator, pose, OUTLINE_COLOR, shape);
+        // pose.popPose();
+        //
+        // RenderSystem.depthMask(true);
+        // RenderSystem.enableDepthTest();
+        // RenderSystem.enableCull();
     }
 
     /**
      * Draws boxes for an outline. Depth and blending should be set before this is called.
      */
     private static void drawOutlineBoxes(Tesselator tessellator, PoseStack matrices, Color color, VoxelShape outline) {
-        PoseStack.Pose entry = matrices.last();
-
-        BufferBuilder buffer = tessellator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-        // Divide into each edge and draw all of them
-        outline.forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> {
-            // Fix Z fighting
-            minX -= .001;
-            minY -= .001;
-            minZ -= .001;
-            maxX += .001;
-            maxY += .001;
-            maxZ += .001;
-            drawBox(entry, buffer, (float) minX, (float) minY, (float) minZ, (float) maxX, (float) maxY, (float) maxZ, color);
-        });
-        BufferUploader.drawWithShader(buffer.buildOrThrow());
+        // Block highlighting is disabled.
+        // PoseStack.Pose entry = matrices.last();
+        //
+        // BufferBuilder buffer = tessellator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+        // // Divide into each edge and draw all of them
+        // outline.forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> {
+        //     // Fix Z fighting
+        //     minX -= .001;
+        //     minY -= .001;
+        //     minZ -= .001;
+        //     maxX += .001;
+        //     maxY += .001;
+        //     maxZ += .001;
+        //     drawBox(entry, buffer, (float) minX, (float) minY, (float) minZ, (float) maxX, (float) maxY, (float) maxZ, color);
+        // });
+        // BufferUploader.drawWithShader(buffer.buildOrThrow());
     }
 
     /**
@@ -101,77 +105,80 @@ public final class BlockHighlighter {
      * Before calling blend and depth should be set
      */
     private static void drawOutlineLines(Tesselator tessellator, PoseStack matrices, Color color, VoxelShape outline) {
-        GL11.glEnable(GL11.GL_LINE_SMOOTH);
-        RenderSystem.setShader(GameRenderer::getRendertypeLinesShader);
-        RenderSystem.lineWidth(OUTLINE_WIDTH);
-
-        BufferBuilder buffer = tessellator.begin(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL);
-        drawOutlineLine(matrices.last(), buffer, color, outline);
-
-        // Revert some changes
-        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        // Block highlighting is disabled.
+        // GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        // RenderSystem.setShader(GameRenderer::getRendertypeLinesShader);
+        // RenderSystem.lineWidth(OUTLINE_WIDTH);
+        //
+        // BufferBuilder buffer = tessellator.begin(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL);
+        // drawOutlineLine(matrices.last(), buffer, color, outline);
+        //
+        // // Revert some changes
+        // GL11.glDisable(GL11.GL_LINE_SMOOTH);
     }
 
     /**
      * Draws an outline. Setup should be done before this method is called.
      */
     private static void drawOutlineLine(PoseStack.Pose entry, BufferBuilder buffer, Color color, VoxelShape outline) {
-        outline.forAllEdges((minX, minY, minZ, maxX, maxY, maxZ) -> {
-            // Fix Z fighting
-            minX -= .001;
-            minY -= .001;
-            minZ -= .001;
-            maxX += .001;
-            maxY += .001;
-            maxZ += .001;
-            drawLine(entry, buffer, new Vector3d(minX, minY, minZ), new Vector3d(maxX, maxY, maxZ), color);
-        });
-        BufferUploader.drawWithShader(buffer.buildOrThrow());
+        // Block highlighting is disabled.
+        // outline.forAllEdges((minX, minY, minZ, maxX, maxY, maxZ) -> {
+        //     // Fix Z fighting
+        //     minX -= .001;
+        //     minY -= .001;
+        //     minZ -= .001;
+        //     maxX += .001;
+        //     maxY += .001;
+        //     maxZ += .001;
+        //     drawLine(entry, buffer, new Vector3d(minX, minY, minZ), new Vector3d(maxX, maxY, maxZ), color);
+        // });
+        // BufferUploader.drawWithShader(buffer.buildOrThrow());
     }
 
     private static void drawBox(PoseStack.Pose entry, BufferBuilder buffer, float minX, float minY, float minZ, float maxX, float maxY, float maxZ, Color color) {
-        Matrix4f position = entry.pose();
-
-        float r = color.red();
-        float g = color.green();
-        float b = color.blue();
-        float a = color.alpha();
-
-        // West
-        buffer.addVertex(position, minX, minY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, minY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, maxY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, maxY, minZ).setColor(r, g, b, a);
-
-        // East
-        buffer.addVertex(position, maxX, minY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, minY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, maxY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, maxY, maxZ).setColor(r, g, b, a);
-
-        // North
-        buffer.addVertex(position, maxX, minY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, minY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, maxY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, maxY, minZ).setColor(r, g, b, a);
-
-        // South
-        buffer.addVertex(position, minX, minY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, minY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, maxY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, maxY, maxZ).setColor(r, g, b, a);
-
-        // Top
-        buffer.addVertex(position, minX, maxY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, maxY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, maxY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, maxY, minZ).setColor(r, g, b, a);
-
-        // Bottom
-        buffer.addVertex(position, maxX, minY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, minY, maxZ).setColor(r, g, b, a);
-        buffer.addVertex(position, minX, minY, minZ).setColor(r, g, b, a);
-        buffer.addVertex(position, maxX, minY, minZ).setColor(r, g, b, a);
+        // Block highlighting is disabled.
+        // Matrix4f position = entry.pose();
+        //
+        // float r = color.red();
+        // float g = color.green();
+        // float b = color.blue();
+        // float a = color.alpha();
+        //
+        // // West
+        // buffer.addVertex(position, minX, minY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, minY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, maxY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, maxY, minZ).setColor(r, g, b, a);
+        //
+        // // East
+        // buffer.addVertex(position, maxX, minY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, minY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, maxY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, maxY, maxZ).setColor(r, g, b, a);
+        //
+        // // North
+        // buffer.addVertex(position, maxX, minY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, minY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, maxY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, maxY, minZ).setColor(r, g, b, a);
+        //
+        // // South
+        // buffer.addVertex(position, minX, minY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, minY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, maxY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, maxY, maxZ).setColor(r, g, b, a);
+        //
+        // // Top
+        // buffer.addVertex(position, minX, maxY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, maxY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, maxY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, maxY, minZ).setColor(r, g, b, a);
+        //
+        // // Bottom
+        // buffer.addVertex(position, maxX, minY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, minY, maxZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, minX, minY, minZ).setColor(r, g, b, a);
+        // buffer.addVertex(position, maxX, minY, minZ).setColor(r, g, b, a);
     }
 
     /**
@@ -202,19 +209,20 @@ public final class BlockHighlighter {
      * @param color  Color to render
      */
     private static void drawLine(PoseStack.Pose entry, BufferBuilder buffer, Vector3d start, Vector3d end, Color color) {
-        Vector3d normal = getNormalAngle(start, end);
-        float red = color.red();
-        float green = color.green();
-        float blue = color.blue();
-        float alpha = color.alpha();
-
-        buffer.addVertex(entry.pose(), (float) start.x, (float) start.y, (float) start.z)
-            .setColor(red, green, blue, alpha)
-            .setNormal(entry, (float) normal.x, (float) normal.y, (float) normal.z);
-
-        buffer.addVertex(entry.pose(), (float) end.x, (float) end.y, (float) end.z)
-            .setColor(red, green, blue, alpha)
-            .setNormal(entry, (float) normal.x, (float) normal.y, (float) normal.z);
+        // Block highlighting is disabled.
+        // Vector3d normal = getNormalAngle(start, end);
+        // float red = color.red();
+        // float green = color.green();
+        // float blue = color.blue();
+        // float alpha = color.alpha();
+        //
+        // buffer.addVertex(entry.pose(), (float) start.x, (float) start.y, (float) start.z)
+        //     .setColor(red, green, blue, alpha)
+        //     .setNormal(entry, (float) normal.x, (float) normal.y, (float) normal.z);
+        //
+        // buffer.addVertex(entry.pose(), (float) end.x, (float) end.y, (float) end.z)
+        //     .setColor(red, green, blue, alpha)
+        //     .setNormal(entry, (float) normal.x, (float) normal.y, (float) normal.z);
     }
 
     public record Color(float red, float green, float blue, float alpha) {
@@ -225,4 +233,3 @@ public final class BlockHighlighter {
 
     private BlockHighlighter() {}
 }
-
